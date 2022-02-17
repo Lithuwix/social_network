0ef1745e747d83256c861b7a4166db47a1170b03
toki <loremlisumdol@gmail.com>
2022-02-18 02:49:41 +0300

    make array for persons in dialogs

--- src/components/Dialogs/Friend_List/Friend_List.jsx
+++ src/components/Dialogs/Friend_List/Friend_List.jsx
@@ -3,15 +3,24 @@ import css from './Friend_List.module.css';
 import Person_Message from "./Person_Message/Person_Message";
 
 const Friend_List = () => {
+    let dialogsData = [
+        {id: 1, name: 'Paul'},
+        {id: 2, name: 'Doom'},
+        {id: 3, name: 'Flake'},
+        {id: 4, name: 'Oliver'},
+        {id: 5, name: 'Richard'},
+        {id: 6, name: 'Till'},
+
+    ]
     return (
         <div className={css.container}>
             <div className={css.dialogs}>Dialogs</div>
-            <Person_Message name="Paul" chat_id="1"/>
-            <Person_Message name="Doom" chat_id="2"/>
-            <Person_Message name="Flake" chat_id="3"/>
-            <Person_Message name="Oliver" chat_id="4"/>
-            <Person_Message name="Richard" chat_id="5"/>
-            <Person_Message name="Till" chat_id="6"/>
+            <Person_Message name={dialogsData[0].name} chat_id={dialogsData[0].id}/>
+            <Person_Message name={dialogsData[1].name} chat_id={dialogsData[1].id}/>
+            <Person_Message name={dialogsData[2].name} chat_id={dialogsData[2].id}/>
+            <Person_Message name={dialogsData[3].name} chat_id={dialogsData[3].id}/>
+            <Person_Message name={dialogsData[4].name} chat_id={dialogsData[4].id}/>
+            <Person_Message name={dialogsData[5].name} chat_id={dialogsData[5].id}/>
         </div>
     )
 }
